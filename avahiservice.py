db2f53d4707a95903ff3c3f4e7b4c250b1131d34
William Dignazio <slackwill@csh.rit.edu>
2012-04-25 10:29:16 -0400

    Added some preliminary service functions for the avahi service, should work fine on a system running dbus and avahi.

--- avahiservice.py
+++ avahiservice.py
@@ -1,22 +1,57 @@
 """ 
-	Avahi Network Service Scripting
+    Avahi Network Service Scripting
 """
 
 import avahi
 import dbus 
 
-class QuiltAvahiService(object): 
-	""" Quilt's Zeroconf Network Service 
-		NOTE: Ports 9375-9379 should be our target ports
-	"""
-	def __init__(self, name="Quilt", port=9375, stype="_http._tcp", 
-					domain="", host="", text=""): 
-			self.name = name
-			self.stype = stype
-			self.domain = domain
-			self.host = host
-			self.port = port
-			self.text = text
+__all__ = ["QuiltAvahiService"]
 
-	def publish(self): 
-		pass		
+class QuiltAvahiService(object): 
+    """ Quilt's Zeroconf Network Service 
+        NOTE: Ports 9375-9379 should be our target ports
+    """
+    def __init__(self, name="Quilt", port=9375, stype="_ftp._tcp", 
+                    domain="", host="", text=""): 
+            self.name = name
+            self.stype = stype
+            self.domain = domain
+            self.host = host
+            self.port = port
+            self.text = text
+
+    def publish(self): 
+        bus = dbus.SystemBus()
+        server = dbus.Interface(
+                bus.get_object(
+                    avahi.DBUS_NAME, 
+                    avahi.DBUS_PATH_SERVER), 
+                avahi.DBUS_INTERFACE_SERVER)
+
+        interface = dbus.Interface(
+                bus.get_object(avahi.DBUS_NAME, 
+                    server.EntryGroupNew()), 
+                avahi.DBUS_INTERFACE_ENTRY_GROUP)
+        
+        interface.AddService(
+                avahi.IF_UNSPEC, 
+                avahi.PROTO_UNSPEC, 
+                dbus.UInt32(0), 
+                self.name, 
+                self.stype, 
+                self.domain, 
+                self.host, 
+                dbus.UInt16(self.port), 
+                self.text)
+
+        interface.Commit()
+        self.group = interface
+
+    def unpublish(self): 
+        self.group.Reset()
+
+if __name__ == "__main__": 
+    serv = QuiltAvahiService()
+    serv.publish()
+    raw_input("Press a key to kill...")
+    serv.unpublish()
