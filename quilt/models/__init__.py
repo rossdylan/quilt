bb56595ab98ced3d932aee05ec7e8d12146ade8a
Ross Delinger <rossdylan@csh.rit.edu>
2012-05-14 10:50:30 -0400

    combined channel.py and user.py into models/__init__.py to simplify things

--- quilt/models/__init__.py
+++ quilt/models/__init__.py
@@ -1,4 +1,31 @@
 
+class QuiltChannel(object):
+    """
+    Object for storing channel information
+
+    :type channel: str
+    :param channel: name of the channel
+    """
+
+    def __init__(self, channel):
+        self.channe_name = channel
+        self.users = []
+
+    def addUser(self, user):
+        if not user in self.users:
+            self.users.append(user)
+            return True
+        else:
+            return False
+
+    def removeUser(self, user):
+        try:
+            self.users.remove(user)
+            return True
+        except:
+            return False
+
+
 class QuiltUser(object):
     """
     Object to store data related to the User on this quilt node
